340e194cc909b45d1b043e21ce91605f541e11e4
Alfred Radu <alfred.radu@gmail.com>
2023-06-04 14:56:08 +0200

    tables+randomspawn

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,43 +1,28 @@
 extends Node2D
 
-enum FoodType { VEGETABLE, PROTEIN, HERB }
-
-class FoodStats:
-	var filling: int
-	var power: int
-	var taste: int
-
-class FoodItem:
-	var name: String
-	var type: FoodType
-	var stats: FoodStats
-	var ttl: int
-	var inSoup: bool
-	var hiddenCombo: String
-	
 var rng = RandomNumberGenerator.new()
 
 func setupFoodItems(FoodItems):
-	FoodItems.append(getFoodItem("Carrot", FoodType.VEGETABLE))
-	FoodItems.append(getFoodItem("Carrot", FoodType.VEGETABLE))
-	FoodItems.append(getFoodItem("Carrot", FoodType.VEGETABLE))
-	FoodItems.append(getFoodItem("Potato", FoodType.VEGETABLE))
-	FoodItems.append(getFoodItem("Potato", FoodType.VEGETABLE))
-	FoodItems.append(getFoodItem("Potato", FoodType.VEGETABLE))
-	FoodItems.append(getFoodItem("Cabbage", FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Carrot", Globals.FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Carrot", Globals.FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Carrot", Globals.FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Potato", Globals.FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Potato", Globals.FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Potato", Globals.FoodType.VEGETABLE))
+	FoodItems.append(getFoodItem("Cabbage", Globals.FoodType.VEGETABLE))
 #	FoodItems.append(getFoodItem("Beef", FoodType.PROTEIN))
 #	FoodItems.append(getFoodItem("Chicken", FoodType.PROTEIN))
 #	FoodItems.append(getFoodItem("Thyme", FoodType.HERB))
 #	FoodItems.append(getFoodItem("Parsley", FoodType.HERB))
 	
-func getFoodItem(itemName, type, stats: FoodStats = null):
-	var item = FoodItem.new()
+func getFoodItem(itemName, type, stats: Globals.FoodStats = null):
+	var item = Globals.FoodItem.new()
 	item.name = itemName
 	item.type = type
 	item.ttl = rng.randf_range(60, 120)
 	item.hiddenCombo = getHiddenCombo(itemName)
 	
-	var itemStats = FoodStats.new()
+	var itemStats = Globals.FoodStats.new()
 	itemStats.filling = stats.filling if stats != null else getFilling(type)
 	itemStats.power = stats.power if stats != null else getPower(type)
 	itemStats.taste = stats.taste if stats != null else getTaste(type)
@@ -47,27 +32,27 @@ func getFoodItem(itemName, type, stats: FoodStats = null):
 	return item
 	
 func getFilling(type):
-	if (type == FoodType.VEGETABLE):
+	if (type == Globals.FoodType.VEGETABLE):
 		return rng.randi_range(70, 100)
-	if (type == FoodType.PROTEIN):
+	if (type == Globals.FoodType.PROTEIN):
 		return rng.randi_range(30, 80)
-	if (type == FoodType.HERB):
+	if (type == Globals.FoodType.HERB):
 		return rng.randi_range(0, 20)
 
 func getPower(type):
-	if (type == FoodType.VEGETABLE):
+	if (type == Globals.FoodType.VEGETABLE):
 		return rng.randi_range(30, 50)
-	if (type == FoodType.PROTEIN):
+	if (type == Globals.FoodType.PROTEIN):
 		return rng.randi_range(60, 100)
-	if (type == FoodType.HERB):
+	if (type == Globals.FoodType.HERB):
 		return rng.randi_range(20, 30)
 
 func getTaste(type):
-	if (type == FoodType.VEGETABLE):
+	if (type == Globals.FoodType.VEGETABLE):
 		return rng.randi_range(10, 80)
-	if (type == FoodType.PROTEIN):
+	if (type == Globals.FoodType.PROTEIN):
 		return rng.randi_range(40, 100)
-	if (type == FoodType.HERB):
+	if (type == Globals.FoodType.HERB):
 		return rng.randi_range(70, 100)
 		
 func getHiddenCombo(itemName):
