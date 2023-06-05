00eda2bb5c06ece190d8f22987a8e12655bf42b0
Alfred Radu <alfred.radu@gmail.com>
2023-06-05 23:36:08 +0200

    book2

---
+++ Scripts/bookRecipOpen.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_button_down():
+	pass # Replace with function body.
+	#TURN PAGE
+
+
+func _on_backbtn_button_down():
+	self.visible = false;
