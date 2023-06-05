c0742cbad707bf80c3b33f2be7bce3448316713e
Alfred Radu <alfred.radu@gmail.com>
2023-06-05 15:14:55 +0200

    armed

--- Scripts/hand.gd
+++ Scripts/hand.gd
@@ -1,21 +1,38 @@
 extends Node2D
 
-@onready var sprite = $Sprite2D
+@onready var sprite = $handhand
+@onready var arm = $armhand
 
 var sprite_open = load("res://Assets/hand/hand-open.png")
 var sprite_closed = load("res://Assets/hand/hand-closed.png")
 
 var state = Globals.handState.OPEN
 var pickedItem
-
+var expansion = 0
+var armScaleY
+var armScaleX
+var armY
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	armScaleY = arm.scale.y
+	armScaleX = arm.scale.x
+	armY = arm.position.y
 	$HoverText.text = ""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	var mousepos = get_viewport().get_mouse_position()
-	self.position = Vector2(mousepos.x, mousepos.y)
+	var viewportRect = get_viewport().get_visible_rect()
+	if mousepos.x < viewportRect.size.x && mousepos.x > 0 && mousepos.y < viewportRect.size.y && mousepos.y > 0:
+		self.position = Vector2(mousepos.x, mousepos.y)
+		if viewportRect.size.y-mousepos.y > 700:
+			expansion = (viewportRect.size.y-mousepos.y-700)/1000
+		else:
+			expansion = 0
+		arm.scale.y = armScaleY + expansion * 4
+		arm.scale.x = armScaleX - expansion
+		arm.position.y = armY + expansion * 1000
+		print(arm.position.y)
 	
 func _input(event):
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
